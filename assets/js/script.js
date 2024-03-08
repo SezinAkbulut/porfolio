56a84869ecdd635c4956bc0f328c700d596893ad
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-08 20:55:47 +0100

    add clip-path between night and day mood

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,11 @@ const container = document.querySelector(".container");
 
 const cloneContainer = container.cloneNode(true);
 cloneContainer.id = "dark-container";
+document.body.appendChild(cloneContainer);
+cloneContainer.classList.remove("active");
 
 console.log(cloneContainer);
 
-document.body.appendChild(cloneContainer);
+const darkContainer = document.querySelector("#dark-container .home-img img");
+
+darkContainer.src = "assets/img/home-perfil.png";
