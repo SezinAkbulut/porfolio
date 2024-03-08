1f2bca60c9d054b1b56bde760972058bcb75c3b5
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-08 21:09:43 +0100

    add day mood button to toggle with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,19 @@ document.body.appendChild(cloneContainer);
 cloneContainer.classList.remove("active");
 
 console.log(cloneContainer);
-
-const darkContainer = document.querySelector("#dark-container .home-img img");
+const toggleIcons = document.querySelectorAll(".toggle-icon");
+const icons = document.querySelectorAll(".toggle-icon i");
+const darkContainer = document.querySelectorAll("#dark-container");
+const darkContainerImg = document.querySelector(
+  "#dark-container .home-img img"
+);
 
 darkContainer.src = "assets/img/home-perfil.png";
+
+toggleIcons.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    icons.forEach((icon) => {
+      icon.classList.toggle("bx-sun");
+    });
+  });
+});
