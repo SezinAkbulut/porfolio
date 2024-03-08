8d9b7f0a870b85344d475d5e171e2403fb09fb4e
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-08 21:58:43 +0100

    update toggle to add transition effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,22 @@ darkContainer.src = "assets/img/home-perfil.png";
 
 toggleIcons.forEach((toggle) => {
   toggle.addEventListener("click", () => {
+    toggle.classList.add("disabled");
+    setTimeout(() => {
+      toggle.classList.remove("disabled");
+    }, 1500);
+
     icons.forEach((icon) => {
       icon.classList.toggle("bx-sun");
     });
+
+    container.classList.toggle("active");
+    cloneContainer.classList.toggle("active");
+
+    if (cloneContainer.classList.contains("active")) {
+      cloneContainer.style.clipPath = "circle(150% at 0 0)";
+    } else {
+      cloneContainer.style.clipPath = "circle(0% at 0 0)";
+    }
   });
 });
