8081f024072bbb403ae2b034b681dd5042a23f56
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-19 14:13:27 +0100

    add contact page

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,16 +1,15 @@
 const container = document.querySelector(".container");
+
 const cloneContainer = container.cloneNode(true);
 cloneContainer.id = "dark-container";
 document.body.appendChild(cloneContainer);
 cloneContainer.classList.remove("active");
 
+console.log(cloneContainer);
 const toggleIcons = document.querySelectorAll(".toggle-icon");
 const icons = document.querySelectorAll(".toggle-icon i");
-const darkContainerImg = document.querySelector(
-  "#dark-container .home-img img"
-);
+const darkContainer = document.querySelectorAll("#dark-container");
 
-darkContainerImg.style.backgroundColor = "#15202b";
 toggleIcons.forEach((toggle) => {
   toggle.addEventListener("click", () => {
     toggle.classList.add("disabled");
@@ -27,10 +26,26 @@ toggleIcons.forEach((toggle) => {
 
     if (cloneContainer.classList.contains("active")) {
       cloneContainer.style.clipPath = "circle(150% at 0 0)";
-      darkContainerImg.style.backgroundColor = "#ffffff";
     } else {
       cloneContainer.style.clipPath = "circle(0% at 0 0)";
-      darkContainerImg.style.backgroundColor = "#15202b";
     }
   });
+});
+
+document.getElementById("send-message").addEventListener("click", function () {
+  var name = document.getElementById("name").value.replace(/ /g, "%20");
+  var email = document.getElementById("email").value.replace(/@/g, "%40");
+  var message = document.getElementById("message").value.replace(/ /g, "%20");
+  var subject = encodeURIComponent("New Message from Portfolio Contact Form");
+  var body =
+    "Name: " +
+    name +
+    "%0D%0A" +
+    "Email: " +
+    email +
+    "%0D%0A" +
+    "Message: " +
+    message;
+  window.location.href =
+    "mailto:sezin.akbulut@gmail.com?subject=" + subject + "&body=" + body;
 });
